e98befaecb9d286a3cbf8aa8ca619e62dc25b14c
ae <alex@gzcim.com>
2025-09-29 14:33:45 +0200

    fix(portfolio): remove duplicate named export causing Vite/esbuild conflict

--- Main_Frontend/src/components/portfolio/index.ts
+++ Main_Frontend/src/components/portfolio/index.ts
@@ -1,7 +1,2 @@
 export { Portfolio } from './Portfolio'
 export { default } from './Portfolio'
-
-export { Portfolio } from './Portfolio'
-export { PortfolioTable } from './PortfolioTable'
-export { PortfolioMetrics } from './PortfolioMetrics'
-export { PortfolioChart } from './PortfolioChart'
